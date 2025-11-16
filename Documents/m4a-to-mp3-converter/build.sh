6aae7cc2d787ccf4d952d414a8812ccb19922d77
Surya V <krishnasurya2011@gmail.com>
2025-11-16 22:41:34 +0530

    Update build.sh

--- Documents/m4a-to-mp3-converter/build.sh
+++ Documents/m4a-to-mp3-converter/build.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
+set -e
+
+# Install FFmpeg on Render server
+apt-get update
+apt-get install -y ffmpeg
+
+# Build app
 chmod +x mvnw
-./mvnw clean package -DskipTests
+./mvnw clean package -DskipTests=true
+
+# Run app
+java -jar target/*.jar
